0b06dfaf67f5558384892d20224f4f867cec5e0b
sohamsinh31 <sohamb2019@gmail.com>
2024-01-27 13:18:37 +0530

    dfcds

--- frontend/app/Components/Navbar/Navbar.tsx
+++ frontend/app/Components/Navbar/Navbar.tsx
@@ -1,7 +1,10 @@
+import { Button } from "../Button/Button"
+import "./Navbar.css"
+
 export const Navbar = () => {
     return (
         <div className="navbar">
-            
+            <button>Back</button>
         </div>
     )
-    }
+}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,16 @@
-"use client"
+// "use client"
 import React, { useEffect, useState } from 'react';
 import Sidebar from './Components/Sidebar';
+import Navbar from './Components/Navbar';
 
 export default function App() {
-
   return (
-    <div style={{ minHeight: '100vh' }} >
+    <div style={{ display: 'flex', minHeight: '100vh' }}>
       <Sidebar />
+      <div className="container" style={{ flex: 1, padding: '20px' }}>
+        <Navbar />
+        {/* Other main content goes here */}
+      </div>
     </div>
-  )
+  );
 }
